fb90e609a462d2a4998678a932c7d9d5614d117c
Johan D. Ospina <johan.ospina05@gmail.com>
2022-03-01 16:02:12 +0100

    Service Workers

--- assets/index.js
+++ assets/index.js
@@ -19,3 +19,13 @@ muteButton.onclick = () => {
     player.muted();
   }
 };
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js').then(function(registration) {
+    console.log('Service worker registration succeeded:', registration);
+  }).catch(function(error) {
+    console.log('Service worker registration failed:', error);
+  });
+} else {
+  console.log('Service workers are not supported.');
+}
